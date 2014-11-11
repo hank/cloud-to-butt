c4887c9f00fcdc481c54a6a8c8ec6035fccb4a47
Alec Brickner <alecjbrick@hotmail.com>
2014-11-11 15:37:06 -0800

    "My butt" can no longer be plural, "cloud" is now only changed when referring to data clouds

--- Source/content_script.js
+++ Source/content_script.js
@@ -33,7 +33,7 @@ function handleText(textNode) {
 	var v = textNode.nodeValue;
 
   // Deal with the easy case
-  v = v.replace(/\b(T|t)he (C|c)loud/g, function(match, p1, p2, offset, string) {
+  v = v.replace(/\b(T|t)he (C|c)louds?/g, function(match, p1, p2, offset, string) {
     // t - 7 = m
     // c - 1 = b
     m = String.fromCharCode(p1.charCodeAt(0) - 7);
@@ -50,7 +50,7 @@ function handleText(textNode) {
   // Get the corner cases
   if(v.match(/cloud/i)) {
     // If we're not talking about weather
-    if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
+    if(v.match(/(PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software)/i)) {
       v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
         // c - 1 = b
         b = String.fromCharCode(p1.charCodeAt(0) - 1);
